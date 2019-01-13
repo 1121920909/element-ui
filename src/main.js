03b7aaf6a3e92bddb796e41e1b20be5e43898f92
zyp-mi <1121920909@qq.com>
2019-01-13 15:38:32 +0800

    add admin.vue

--- src/main.js
+++ src/main.js
@@ -9,12 +9,19 @@ import Device from './components/device/Device'
 import Teach from './components/teach/Teach'
 import Index from './components/index'
 import Teacher from './components/teacher'
-import Course from './components/teacher/course/course'
+//import Course from './components/teacher/course/course'
+import Course from './components/teacher/course/course2'
+import classInfo from './components/admin/class'
+import courseInfo from  './components/admin/course'
+import deviceInfo from  './components/admin/device'
+import labInfo from './components/admin/lab'
+import userInfo from './components/admin/user'
+import Admin from './components/admin'
 const routes = [
   {
     path: '/index', component: Index,
     children: [
-      { path: '/', name:'home', component: home},
+      { path: '', name:'home', component: home},
       { path: 'intro', name: 'intro', component: Introduction},
       { path: 'manage', name: 'manage', component: Management},
       { path: 'device', name: 'device', component: Device},
@@ -24,7 +31,17 @@ const routes = [
   {
     path: '/teacher',component: Teacher,
     children: [
-      {path: 'course', name: 'course', component: Course}
+      {path: '', name: 'course', component: Course}
+    ]
+  },
+  {
+    path: '/admin',component: Admin,
+    children: [
+      {path: '',name:'classInfo',component:classInfo},
+      {path: 'course',name:'courseInfo',component:courseInfo},
+      {path: 'device',name:'deviceInfo',component:deviceInfo},
+      {path: 'lab',name:'labInfo',component:labInfo},
+      {path:'user',name:'teacherInfo',component:userInfo}
     ]
   }
 
